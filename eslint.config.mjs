0c0b120fa2899cec7145faab2c82a20352f94648
Kyle Surowiec <kylesurowiec@me.com>
2024-07-07 02:41:28 -0600

    fmt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,80 @@
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "dist",
-        "node_modules",
-        "example",
-        "**/.eslintrc.js",
-        "**/rollup.config.ts",
+      'dist',
+      'node_modules',
+      'example',
+      '**/.eslintrc.js',
+      '**/rollup.config.ts',
     ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/strict",
-    "prettier",
-), {
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+  ),
+  {
     ignores: [
-        '/dist',
-        '/node_modules',
-        'rollup.config.ts',
+      '/dist',
+      '/node_modules',
+      'rollup.config.ts',
+      'eslint.config.mjs',
     ],
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-        parserOptions: {
-            project: "tsconfig.eslint.json",
-        },
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        project: 'tsconfig.eslint.json',
+      },
     },
     rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
-        "@typescript-eslint/explicit-member-accessibility": ["warn", {
-            accessibility: "explicit",
-            overrides: {
-                accessors: "explicit",
-                constructors: "no-public",
-                methods: "explicit",
-                properties: "off",
-            },
-        }],
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-floating-promises": ["warn", {
-            ignoreVoid: true,
-            ignoreIIFE: true,
-        }],
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-        }],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'warn',
+        {
+          accessibility: 'explicit',
+          overrides: {
+            accessors: 'explicit',
+            constructors: 'no-public',
+            methods: 'explicit',
+            properties: 'off',
+          },
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-floating-promises': [
+        'warn',
+        {
+          ignoreVoid: true,
+          ignoreIIFE: true,
+        },
+      ],
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
-}];
+  },
+];
